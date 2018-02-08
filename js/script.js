850137c1af73ee91cce447c647e32ab7fcc9ea32
Joram Livengood <joram.livengood@gmail.com>
2018-02-08 12:33:14 -0600

    Work work work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-$('video').mediaelementplayer( {
-  features: ['playpause', 'progress', 'volume', 'fullscreen'],
+const $video = $('.video');
+const $span = $('span');
+let $currentTime;
+
+$video.mediaelementplayer( {
+  videoWidth: '100%',
+  videoHeight: '100%',
+  features: ['playpause', 'progress', 'tracks', 'volume', 'fullscreen'],
 })
+
+for (let i = 0; i < $span.length; i++) {
+  let dataStart = $span.attr('data-start');
+  console.log(dataStart);
+}
+
+$video.on('timeupdate', () => {
+  $currentTime = $video.prop('currentTime');
+  console.log($currentTime);
+})
+
+console.log($span.attr('data-start'));
