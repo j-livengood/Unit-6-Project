65e63c9f8bb58f66955407d462f6765c6ce3adb0
Joram Livengood <joram.livengood@gmail.com>
2018-02-08 18:03:33 -0600

    Work some more
    Writing JS for functionality

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
-const $video = $('.video');
-const $span = $('span');
-let $currentTime;
+const video = document.querySelector('.video');
+const span = document.querySelectorAll('span');
+const transcript = document.querySelector('.transcript');
 
-$video.mediaelementplayer( {
+const videoPlayer = new MediaElementPlayer(video, {
+  pluginPath: "https://cdnjs.cloudflare.com/ajax/libs/mediaelement/4.2.7/mediaelement-and-player.min.js",
+  shimScriptAccess: 'always',
   videoWidth: '100%',
   videoHeight: '100%',
-  features: ['playpause', 'progress', 'tracks', 'volume', 'fullscreen'],
-})
+  features: ['playpause', 'progress', 'tracks', 'volume', 'fullscreen']
+});
 
-for (let i = 0; i < $span.length; i++) {
-  let dataStart = $span.attr('data-start');
-  console.log(dataStart);
-}
+video.addEventListener('timeupdate', () => {
+  for (let i = 0; i < span.length; i++) {
+    let currentTime = video.currentTime;
+    let dataStart = span[i].getAttribute('data-start');
+    let dataEnd = span[i].getAttribute('data-end');
+    
+    if (dataStart <= currentTime && dataEnd >= currentTime) {
+      span[i].classList.add('playing');
+    } else if (dataEnd < currentTime) {
+      span[i].className = 'played';
+    } else {
+      span[i].className = '';
+    }
+  }
+});
 
-$video.on('timeupdate', () => {
-  $currentTime = $video.prop('currentTime');
-  console.log($currentTime);
-})
-
-console.log($span.attr('data-start'));
